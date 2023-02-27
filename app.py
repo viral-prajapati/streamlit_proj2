e7d0297412526b177c12e2c7b751feaf564446a1
Viral Prajapati <72140053+viral-prajapati@users.noreply.github.com>
2023-02-27 14:26:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("Hello! I am Viral Prajapati")
+st.write('''Hi there! I'm Viral Prajapati. I have completed my bachelor's in Information Technology from Government Engineering College, Gandhinagar with an 8.65 CGPA, affiliated with Gujarat Technological University. I'm very interested in industry 4.0 technologies like AI, Machine Learning, Deep Learning, and Blockchain. In addition to having the highest average GPA in my class, I currently work as a Python developer at Code Space Techlabs and have completed two industrial internships, where I gained part-time corporate experience.''')
+
+st.subheader('Skills & Tools ⚒️')
+col1, col2, col3 = st.columns(3)
+
+col1.write('Python')
+col1.write('Streamlit')
+col1.write('Artificial Intelligence')
+
+col2.write('ReactJs')
+col2.write('Git & GitHub')
+col2.write('Machine Learning')
+
+col3.write('Django')
+col3.write('Jupyter Notebook')
+col3.write('Data Science')
+
+st.subheader('I have created data visualization on Titanic Dataset')
